b05d452d8c8deddf2aa0167576268499f7c3a477
tomasz-jedrzejczyk <tomasz.jedrzejczyk.dev@gmail.com>
2025-10-21 19:38:44 +0000

    test commit from ec2

---
+++ django-app/home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'title': 'Welcome to My Django App',
+        'message': 'Successfully deployed with Docker and GitHub Actions!',
+    }
+    return render(request, 'home/index.html', context)

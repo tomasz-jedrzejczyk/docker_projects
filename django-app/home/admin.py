e7bfdd8c966927570166221d00a4a2ecc63acb7d
tomasz-jedrzejczyk <tomasz.jedrzejczyk.dev@gmail.com>
2025-10-29 21:46:35 +0100

    add admin panel and user management system, add blog posting

--- django-app/home/admin.py
+++ django-app/home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import BlogPost
 
-# Register your models here.
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date', 'published_date')
+    search_fields = ('title', 'content')
+    list_filter = ('published_date', 'created_date')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'created_date'
